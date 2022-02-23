33f49cfa8433956cb00847bed4ae3f2db09ab3b1
vimalkmr816 <vimalkmr816@gmail.com>
2022-02-23 22:02:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Meme from "./Meme";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [templates, setTemplates] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(data => {
+                setTemplates(data.data.memes);
+            })
+
+    }, []);
+    const styles={
+        border:'2px solid'
+    }
+    return (
+        <div className="App">
+            <h1>Meme Generator</h1>
+            <Meme value={templates}/>
+        </div >
+    );
 }
 
 export default App;
---
+++ src/Meme.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Meme({ props }) {
+    return (
+        <div>{props}</div>
+    )
+}
+
+export default Meme
